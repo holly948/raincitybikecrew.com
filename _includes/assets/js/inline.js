6e18e27456df0c8a902dfa030466d7cb1f61f0fc
Bart Nagel <bart@tremby.net>
2021-05-26 22:46:02 -0700

    Add fallback styles for Safari <14.1 (no flex gap support)

--- _includes/assets/js/inline.js
+++ _includes/assets/js/inline.js
@@ -7,3 +7,27 @@ if (window.netlifyIdentity) {
     }
   });
 }
+
+// Based on
+// https://github.com/Modernizr/Modernizr/blob/master/feature-detects/css/flexgap.js
+// (MIT licence)
+function detectFlexGap() {
+  // Create flex container with row-gap set
+  const flex = document.createElement("div");
+  flex.style.display = "flex";
+  flex.style.flexDirection = "column";
+  flex.style.rowGap = "1px";
+
+  // Create two elements inside it
+  flex.appendChild(document.createElement("div"));
+  flex.appendChild(document.createElement("div"));
+
+  // Append to the DOM (needed to obtain scrollHeight)
+  document.body.appendChild(flex);
+  const isSupported = flex.scrollHeight === 1;
+  flex.parentNode?.removeChild(flex);
+
+  return isSupported;
+}
+
+document.documentElement.classList.add(detectFlexGap() ? "flex-gap" : "no-flex-gap");
